c154ed980abb91e934b6b5ea3df6578128c39cd4
sreepathysois <54054574+sreepathysois@users.noreply.github.com>
2021-04-02 12:00:44 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! and welcome to devops";
+    private final String message = "Hello World!";
 
     public App() {}
 
